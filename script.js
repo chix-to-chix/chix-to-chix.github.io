5daab096f35ad1d07fa99b504832039ac24d9ce0
chix-to-chix <salazarlesterdave@gmail.com>
2025-05-25 12:43:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ export const menuItems = [
   { name: "Extra Java", category: "Extras", price: "₱30.00", description: "Java rice", image: "java.png" },
   { name: "Cheesy Overload Fries", category: "Extras", price: "₱59.00", description: "Overload fries with cheese", image: "fries.png" },
   { name: "Extra Sauce", category: "Extras", price: "₱25.00", description: "Special chicken sauce", image: "sauces.png" },
-  { name: "Garlic Mayo Dip", category: "Extras", price: "₱30.00", description: "Garlic Mayo dipping sauce", image: "sample.jpg" }
+  { name: "Garlic Mayo Dip", category: "Extras", price: "₱30.00", description: "Garlic Mayo dipping sauce", image: "mayo.png" }
 ];
 
 export const unliPrice = 299.00;
